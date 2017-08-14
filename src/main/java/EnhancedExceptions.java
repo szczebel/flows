06bde13080022cf67fb608e5b93055c934a4c31f
Adam_Szczeblewski <Adam_Szczeblewski@epam.com>
2017-08-14 13:04:48 +0200

    changes

--- src/main/java/EnhancedExceptions.java
+++ src/main/java/EnhancedExceptions.java
@@ -3,11 +3,16 @@ import static fluentconditionals.FluentConditionals.*;
 //Task 9
 public class EnhancedExceptions {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CustomException {
+        when(false)
+                .then(doNothing)
+                .orElseThrow(CustomException::new);
+        //Exception in thread "main" EnhancedExceptions$CustomException
+
         given("Greetings")
                 .when(TestHelper::somethingIsTrue)
                 .thenReturn(String::hashCode)
-                .orElseThrow(RuntimeException::new, "Exception message");
+                .orElseThrow(CustomException::new, "Exception message");
 
         when(TestHelper::somethingIsTrue)
                 .then(doNothing)
@@ -16,5 +21,14 @@ public class EnhancedExceptions {
         when(TestHelper::somethingIsTrue)
                 .thenThrow(RuntimeException::new, "This was expected");
         //Exception in thread "main" java.lang.RuntimeException: This was expected
+    }
+
+    static class CustomException extends Exception {
+        public CustomException() {
+        }
+
+        public CustomException(String message) {
+            super(message);
+        }
     }
 }
