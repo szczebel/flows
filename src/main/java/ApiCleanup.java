5a78c76178850215c6047e1d56b4bf5a7de60e04
Adam_Szczeblewski <Adam_Szczeblewski@epam.com>
2017-12-11 13:04:57 +0100

    task 10

--- src/main/java/ApiCleanup.java
+++ src/main/java/ApiCleanup.java
@@ -27,6 +27,7 @@ public class ApiCleanup {
         int result1 = when(TestHelper::somethingIsTrue)
                 .thenReturn(TestHelper::getHighNumber);
         int result2 = when(true).then(TestHelper::printFoo).orElse(1);
+        int result3 = when(true).then(TestHelper::printFoo).orElse(TestHelper::printBar);
     }
 
     static boolean veryComplexCondition() {
